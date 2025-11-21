bc6085bebcef5d4c5b334881dc11843645597e0a
STRXI01 <rizalpro598@gmail.com>
2025-11-21 06:36:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,15 +34,15 @@ COOKIES = getenv("COOKIES", None)
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/SpaceXe-tech/bx",
+    "https://github.com/STRXI01/Nova",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/BillaSpace")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/BillaCore")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Syphixlabs")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SyphixHub")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
